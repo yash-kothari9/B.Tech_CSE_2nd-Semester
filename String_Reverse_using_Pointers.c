582ef7375f6cbd5e152ab82847bc2073dbf1c544
yash-kothari9 <157686395+yash-kothari9@users.noreply.github.com>
2024-02-01 12:59:28 +0530

    Add files via upload

---
+++ String_Reverse_using_Pointers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+     
+
+    reverseString(str);
+
+    printf("Reversed string: %s", str);
+
+    return 0;
+}
